6ec1c02466f7d141a82204f1628cf301cc5285ec
JVLsx <143802935+JVLsx@users.noreply.github.com>
2024-10-13 16:58:04 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+    'Next_Level':['Next_Level','sans-serif']
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+

382cd3fa8f8b9ebbddc8da0b08f8bb0c9f50d4b8
JVLsx <143802935+JVLsx@users.noreply.github.com>
2024-12-02 11:11:48 -0300

    Update identificacao.js

--- identificacao.js
+++ identificacao.js
@@ -6,8 +6,16 @@ function showIdentification() {
 
     // Verifica se todos os campos foram preenchidos
     if (name && course && year) {
+        location.href = 'confirmacao.html'
+        // guarde as informções em variaveis
+        localStorage.setItem("name", name);
+            
+            localStorage.setItem("course", course);
+                
+                localStorage.setItem("year", year);
+                    
         // Mostra a identificação no output
-        document.getElementById("output").innerHTML = `<strong>Nome:</strong> ${name}<br><strong>Curso:</strong> ${course}<br><strong>Série:</strong> ${year}`;
+       // document.getElementById("output").innerHTML = `<strong>Nome:</strong> ${name}<br><strong>Curso:</strong> ${course}<br><strong>Série:</strong> ${year}`;
     } else {
         // Se algum campo não foi preenchido, exibe uma mensagem de erro
         alert("Por favor, preencha todos os campos.");
